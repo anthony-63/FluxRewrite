e2d20caaf70b29bfb0f19ab73d7aae1f6e233989
anthony-63 <back.up.email.158@gmail.com>
2023-05-23 13:05:04 -0400

    add search in map list

--- scripts/menu/MapList.gd
+++ scripts/menu/MapList.gd
@@ -5,7 +5,19 @@ var map_button_data = []
 func _ready():
 	$MapScrollVbox/MapButton.visible = false
 	update_maps()
-	
+func _process(_d):
+	for child in $MapScrollVbox.get_children():
+		if child.name.begins_with("MapButton_"):
+			if $MapScrollVbox/Search.text == "":
+				child.visible = true
+			elif child.map_data.meta.title.findn($MapScrollVbox/Search.text) != -1:
+				child.visible = true
+			elif child.map_data.meta.mapper.findn($MapScrollVbox/Search.text) != -1:
+				child.visible = true
+			elif child.map_data.meta.artist.findn($MapScrollVbox/Search.text) != -1:
+				child.visible = true
+			else:
+				child.visible = false
 func update_maps():
 	print("Updating maps...")
 	for child in $MapScrollVbox.get_children():
@@ -21,7 +33,7 @@ func update_maps():
 	for data in map_button_data:
 		var button = $MapScrollVbox/MapButton.duplicate()
 		button.name = "MapButton_%d" % i
-		button.visible = true
+		button.visible = false
 		button.update(data)
 		i += 1
 		$MapScrollVbox.add_child(button)
