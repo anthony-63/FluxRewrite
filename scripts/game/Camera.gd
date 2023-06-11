dadb7fce2690998ce178a89bc8b2ad3c2e418bcb
anthony-63 <back.up.email.158@gmail.com>
2023-06-11 18:31:59 -0400

    rewrite spin camera properly

---
+++ scripts/game/Camera.gd
@@ -0,0 +1,14 @@
+extends Node
+
+@export var yaw = 0
+@export var pitch = 0
+
+func _input(event):
+	if event is InputEventMouseMotion and Flux.get_setting("game", "spin"):
+		yaw = fmod(yaw + event.relative.y * Flux.get_setting("cursor", "sensitivity") / 250.0, 360) 
+		pitch = max(min(pitch + event.relative.x * Flux.get_setting("cursor", "sensitivity") / 250.0, 90),-90)
+		$"../Camera3D".rotation_degrees.x = pitch
+		$"../Camera3D".rotation_degrees.y = yaw
+
+		
+#		$"../Camera3D".rotate_y(deg_to_rad(yaw))
--- scripts/game/InvisCursor.gd
+++ scripts/game/InvisCursor.gd
@@ -3,14 +3,16 @@ extends Node
 func _ready():
 	self.scale = Vector3.ONE * Flux.get_setting("cursor", "scale")
 
-func _input(ev):
-	if ev is InputEventMouseMotion:
-		self.transform.origin.x -= (ev.relative.x * (Flux.get_setting("cursor", "sensitivity") / 100))
-		self.transform.origin.y -= (ev.relative.y * (Flux.get_setting("cursor", "sensitivity") / 100))
-		
-#		print("\nclamp coords\nx<{CXA}, {CXB}>\ny<{CYA}, {CYB}>".format({
-#			"CXA": $"../HUD/Border".scale.x * -1.5,
-#			"CXB": $"../HUD/Border".scale.x * 1.5,
-#			"CYA": $"../HUD/Border".scale.y * -1.5,
-#			"CYB": $"../HUD/Border".scale.y * 1.5
-#		}))
+func _process(d):
+	self.transform.origin.x = tan($"../Camera3D".pitch) * $"../Camera3D".transform.origin.z
+	self.transform.origin.y = tan($"../Camera3D".yaw) * $"../Camera3D".transform.origin.z
+
+#func _input(ev):
+#	if ev is InputEventMouseMotion:
+#
+##		print("\nclamp coords\nx<{CXA}, {CXB}>\ny<{CYA}, {CYB}>".format({
+##			"CXA": $"../HUD/Border".scale.x * -1.5,
+##			"CXB": $"../HUD/Border".scale.x * 1.5,
+##			"CYA": $"../HUD/Border".scale.y * -1.5,
+##			"CYB": $"../HUD/Border".scale.y * 1.5
+##		}))
